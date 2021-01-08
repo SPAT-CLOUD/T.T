01d268d019cb910d42e4a93468d97b47c30aba36
PORUSPAT3512 <76798491+PORUSPAT3512@users.noreply.github.com>
2021-01-08 07:17:13 +0530

    Create RedirecterToPage.js

---
+++ RedirecterToPage.js
@@ -0,0 +1,5 @@
+<script>
+function bo(){
+location.href="https://download1580.mediafire.com/9tsm7qolyvdg/cbluylu5vgway1n/Teen+Titans+Go%21To+The+Movies%282018%29+Srikala+Dubbing+Clip+LOW-Quality+720p+BDRip+Telugu+Dubbed+SONY%26PANDU%26ABHI%26HARSHA_DUBBING_FILM.mkv";
+}
+</script>
