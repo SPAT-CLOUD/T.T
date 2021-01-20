b4339de09ff3eed8179ac7636fdca00fc179a302
PORUSPAT3512 <76798491+PORUSPAT3512@users.noreply.github.com>
2021-01-20 19:43:31 +0530

    Update OperatingSystemVerfier.js

--- LINUX/JS/OperatingSystemVerfier.js
+++ LINUX/JS/OperatingSystemVerfier.js
@@ -5,7 +5,7 @@ if(chk == "L"){
 document.getElementById("show").innerHTML="SITE FOR ANDRIOD";
 }
 else{
-window.alert("This Is Not For Linux Operating System Such as Andriod Phone && Iphone\nSupported For Windows Operating Systems");
+window.alert("This Is Not For not Windows & MacOsX Operating System Such as Windows Desktop & IPhone && MacBook\nSupported For Android Operating Systems");
 document.write("Not Supported For Your Operating System".fontsize(80));
 setTimeout("end()",4000);
 }
