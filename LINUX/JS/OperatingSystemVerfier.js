06c27d96aa3492d0b8ee3569816794ba0b11b3e8
sonpanabtej <76798491+SPAT-CLOUD@users.noreply.github.com>
2021-02-01 21:14:02 +0530

    Update OperatingSystemVerfier.js

--- LINUX/JS/OperatingSystemVerfier.js
+++ LINUX/JS/OperatingSystemVerfier.js
@@ -5,8 +5,6 @@ if(chk == "L"){
 document.getElementById("show").innerHTML="SITE FOR ANDRIOD Mobile";
 }
 else{
-window.alert("This Is Not For not Windows & MacOsX Operating System Such as Windows Desktop & IPhone && MacBook\nSupported For Android Operating Systems");
-document.write("Not Supported For Your Operating System".fontsize(80));
-setTimeout("end()",4000);
+location.replace("https://spat-cloud.github.io/data/ERROR.html");
 }
 }
